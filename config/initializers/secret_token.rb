244575221536716b867aec27eb6d061ee7a64f35
Duana Saskia <duana@soundcloud.com>
2013-07-17 08:05:35 +0200

    started project with scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ajaxapp::Application.config.secret_token = '1eb15ebf719ff32d069385f4d0ae15c5ec0228210edb49453a8eb5a9639032005710235a10ed7ad01b64f9fd4ebb9c44caf98a9a9fe5b409db9c73d1a8c3951a'
